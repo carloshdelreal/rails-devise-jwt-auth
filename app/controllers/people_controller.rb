9e33b10b9dfdd899467d5bc166e708bc1cbeb833
Carlos Del Real <carloshdelreal@gmail.com>
2023-06-30 15:10:37 -0500

    feat: CRUD person

---
+++ app/controllers/people_controller.rb
@@ -0,0 +1,53 @@
+class PeopleController < ApplicationController
+  respond_to :json
+  before_action :authenticate_user!
+
+  def create
+    person = Person.new(parse_person)
+
+    if person.save
+      render json: { person: person }, status: :ok
+    else
+      render json: { message: 'the person was not created'}, status: :bad_request
+    end
+  end
+
+  def update
+    person = Person.find_by(id: params['id'])
+
+    if person&.update(parse_person)
+      render json: { person: person }, status: :ok
+    else
+      render json: { message: 'the person was not updated'}, status: :bad_request
+    end
+  end
+
+  def index
+    render json: Person.all
+  end
+
+
+  def destroy
+    person = Person.find_by(id: params['id'])
+    if person&.destroy
+      render json: { person: person }, status: :ok
+    else
+      render json: { message: 'there was an error deleting the person'}, status: :bad_request
+    end
+  end
+
+  def show
+    person = Person.find_by(id: params['id'])
+    if person
+      render json: { person: person }, status: :ok
+    else
+      render json: { message: 'there was an error getting the person'}, status: :bad_request
+    end
+  end
+
+  private
+  
+  def parse_person
+    JSON.parse(request.raw_post)['person']
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  
   resources :planets
+
+  post 'people', to: 'people#create'
+  put 'people/:id', to: 'people#update'
+  get 'people', to: 'people#index'
+  get 'people/:id', to: 'people#show'
+  delete 'people/:id', to: 'people#destroy'
 
   get 'current_user', to: 'current_user#index'
   devise_for :users, path: '', path_names: {
